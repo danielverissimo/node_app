7aebb368a6e0d127f24035d4946b4670ef1999cd
Daniel Eduardo <danyelsanches@gmail.com>
2018-06-18 21:57:11 -0300

    app.js

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 FROM node:alpine
+COPY package*.json /
+COPY routes/* /routes/
+COPY bin/* /bin/
+COPY public/* /public/
+COPY views/* /views/
 RUN npm install
 ADD app.js /app.js
 CMD ["node", "/app.js"]
--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 8080!');
 });
 
 module.exports = app;
